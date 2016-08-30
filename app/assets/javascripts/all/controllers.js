2647f3e2e7fa2f9a7387f7bcee6d543d66a3bf2f
Dane <dane160290@yahoo.de>
2016-08-30 23:53:21 +0200

    refresh first view, geil

--- app/assets/javascripts/all/controllers.js
+++ app/assets/javascripts/all/controllers.js
@@ -1,24 +1,79 @@
 define([], function() {
     'use strict';
 
+    
+    
     var AllCtrl = function($scope, $filter, playRoutes, ngTableParams) {
-        $scope.worlds = [];
-        $scope.world = '';
-        $scope.totalDetected = 0;
-        $scope.totalBanned = 0;
-        $scope.totalDeleted = 0;
-
-        /**
-         * Setup date pickers
-         */
-        $scope.toDate = new Date();
-        // default show data from 14 days ago till now
-        $scope.fromDate = new Date($scope.toDate.getTime() - 14 * 24 * 60 * 60 * 1000);
-
-        // Disable weekend selection
-        $scope.disabled = function(date, mode) {
-            return mode === 'day' && (date.getDay() === 0 || date.getDay() === 6);
+        var myScope = $scope;
+        var getTableParams = function(discipline, sex){
+            return new ngTableParams({
+                page: 1,
+                count: 50,
+                sorting: { complete: 'desc' }
+            }, {
+                getData: function($defer) {
+                    playRoutes.controllers.Player.players(discipline,sex).get().then(function(response) {
+                        $defer.resolve(response.data.rows);
+                    });
+                }
+            });
         };
+        var setValuesOnScope = function(scope, active, ak35, ak50, ak65){
+            $scope.showActive = active;
+            $scope.showAk35 = ak35;
+            $scope.showAk50 = ak50;
+            $scope.showAk65 = ak65;
+        };
+        setValuesOnScope($scope, true, false, false, false);
+        $scope.activesLady = getTableParams('Aktive',0);
+        $scope.activesMen = getTableParams('Aktive',1);
+        $scope.ak35Lady = getTableParams('Ak35',0);
+        $scope.ak35Men = getTableParams('Ak35',1);
+        $scope.ak50Lady = getTableParams('Ak50',0);
+        $scope.ak50Men = getTableParams('Ak50',1);
+        $scope.ak65Lady = getTableParams('Ak65',0);
+        $scope.ak65Men = getTableParams('Ak65',1);
+        var currentIndex = 0;
+        var refreshTable =  function(){
+            console.log(currentIndex);
+            if(currentIndex === 0){
+                setValuesOnScope(myScope, true, false, false, false);
+                myScope.activesLady.reload();
+                myScope.activesMen.reload();
+                currentIndex++;
+                startTimer();
+                return;
+            }
+            if(currentIndex === 1){
+                setValuesOnScope(myScope, false, true, false, false);
+                myScope.ak35Lady.reload();
+                myScope.ak35Men.reload();
+                currentIndex++;
+                startTimer();
+                return;
+            }
+            if(currentIndex === 2){
+                setValuesOnScope(myScope, false, false, true, false);
+                myScope.ak50Lady.reload();
+                myScope.ak50Men.reload();
+                currentIndex++;
+                startTimer();
+                return;
+            }
+            if(currentIndex === 3){
+                setValuesOnScope(myScope, false, false, false, true);
+                myScope.ak65Lady.reload();
+                myScope.ak65Men.reload();
+                currentIndex = 0;
+                startTimer();
+                return;
+            }
+          };
+        var startTimer = function(){
+            setTimeout(refreshTable.bind(this), 10000);
+        };
+        startTimer();
+        // Disable weekend selection
 
         $scope.open = function($event, flag) {
             $event.preventDefault();
@@ -27,48 +82,12 @@ define([], function() {
             $scope[flag] = true;
         };
 
-        $scope.dateOptions = {
-            formatYear: 'yy',
-            startingDay: 1
-        };
-
-        /**
-         * Setup table with content
-         */
-        $scope.tableParams = new ngTableParams({
-            page: 1,
-            count: 10,
-            sorting: {date: 'desc'}
-        }, {
-            total: 0,
-            getData: function($defer, params) {
-                var sortedField = Object.keys(params.sorting()).pop(),
-                    offset = params.page() - 1,
-                    limit = params.count(),
-                    fromDate = $filter('date')($scope.fromDate, 'yyyy-MM-dd'),
-                    toDate = $filter('date')($scope.toDate, 'yyyy-MM-dd'),
-                    world = $scope.world || null,
-                    field = sortedField,
-                    sort = params.sorting()[sortedField];
-
-                playRoutes.controllers.Reports.reports(offset, limit, fromDate, toDate, world, field, sort).get().then(function(response) {
-                    $scope.totalDetected = response.data.totalDetected || 0;
-                    $scope.totalBanned = response.data.totalBanned || 0;
-                    $scope.totalDeleted = response.data.totalDeleted || 0;
-
-                    params.total(response.data.totalRows);
-                    $defer.resolve(response.data.rows);
-                });
-            }
-        });
-
+       
         $scope.reloadTable = function() {
             $scope.tableParams.reload();
         };
 
-        playRoutes.controllers.Reports.worldsList().get().then(function(response) {
-            $scope.worlds = response.data;
-        });
+        
     };
 
     AllCtrl.$inject = ['$scope', '$filter', 'playRoutes', 'ngTableParams'];
